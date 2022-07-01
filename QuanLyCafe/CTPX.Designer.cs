c4ea4984e75d6a84b4dc1004541f356cc14d9187
HuyKhang <18004057@student.vlute.edu.vn>
2022-07-01 21:08:25 +0700

    hoan thanh

---
+++ QuanLyCafe/CTPX.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace QuanLyCafe
+{
+    partial class CTPX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvCTPX = new System.Windows.Forms.DataGridView();
+            this.txtCTPX_MAPX = new System.Windows.Forms.TextBox();
+            this.label37 = new System.Windows.Forms.Label();
+            this.txtCTPX_NX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCTPX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCTPX
+            // 
+            this.dgvCTPX.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvCTPX.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCTPX.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvCTPX.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCTPX.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvCTPX.Location = new System.Drawing.Point(16, 112);
+            this.dgvCTPX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvCTPX.Name = "dgvCTPX";
+            this.dgvCTPX.ReadOnly = true;
+            this.dgvCTPX.RowHeadersWidth = 51;
+            this.dgvCTPX.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCTPX.Size = new System.Drawing.Size(1035, 427);
+            this.dgvCTPX.TabIndex = 2;
+            // 
+            // txtCTPX_MAPX
+            // 
+            this.txtCTPX_MAPX.Enabled = false;
+            this.txtCTPX_MAPX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCTPX_MAPX.Location = new System.Drawing.Point(192, 23);
+            this.txtCTPX_MAPX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCTPX_MAPX.Name = "txtCTPX_MAPX";
+            this.txtCTPX_MAPX.Size = new System.Drawing.Size(161, 30);
+            this.txtCTPX_MAPX.TabIndex = 7;
+            // 
+            // label37
+            // 
+            this.label37.AutoSize = true;
+            this.label37.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label37.Location = new System.Drawing.Point(24, 26);
+            this.label37.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label37.Name = "label37";
+            this.label37.Size = new System.Drawing.Size(135, 25);
+            this.label37.TabIndex = 6;
+            this.label37.Text = "Mã phiếu xuất";
+            // 
+            // txtCTPX_NX
+            // 
+            this.txtCTPX_NX.Enabled = false;
+            this.txtCTPX_NX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCTPX_NX.Location = new System.Drawing.Point(192, 65);
+            this.txtCTPX_NX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCTPX_NX.Name = "txtCTPX_NX";
+            this.txtCTPX_NX.Size = new System.Drawing.Size(225, 30);
+            this.txtCTPX_NX.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Người xuất";
+            // 
+            // CTPX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtCTPX_NX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCTPX_MAPX);
+            this.Controls.Add(this.label37);
+            this.Controls.Add(this.dgvCTPX);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CTPX";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CTPX";
+            this.Load += new System.EventHandler(this.CTPX_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCTPX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCTPX;
+        private System.Windows.Forms.TextBox txtCTPX_MAPX;
+        private System.Windows.Forms.Label label37;
+        private System.Windows.Forms.TextBox txtCTPX_NX;
+        private System.Windows.Forms.Label label1;
+    }
+}
